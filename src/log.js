5fb61165ba3e1f8098a2689412a6af54707411d2
Chris Moran <chris.moran@mica.io>
2023-01-22 10:30:15 -0500

    wip functional config for complex mappings

--- src/log.js
+++ src/log.js
@@ -5,17 +5,36 @@ const config = require('./config');
 
 function transform (entry) {
   const _entry = Object.keys(config.mapping).reduce((result, key) => {
-    const value = config.mapping[key];
-    if (value === '$') {
-      result[key] = _.cloneDeep(entry);
-    } else {
-      result[key] = _.get(entry, value);
-    }
+    const value = mappingContext(key);
+    result[value.key] = value.value(entry);
     return result;
   }, {});
 
   _entry.__$levelFilter = _.invert(config.logLevels)[entry.level];
   return _entry;
+}
+
+function mappingContext(key) {
+  if (typeof config.mapping[key] === 'string' && config.mapping[key].indexOf('return') === 0) {
+    const fn = new Function('context', config.mapping[key]);
+    return {
+      key,
+      value: x => fn(_.get(x, key)),
+    };
+  } else if (typeof config.mapping[key] === 'string') {
+    if (config.mapping[key] === '$') {
+      return {
+        key,
+        value: (x) => _.cloneDeep(x),
+      };
+    } else {
+      return {
+        key,
+        value: (x) => _.get(x, config.mapping[key]),
+      };
+    }
+  }
+  return { key, value: (x) => x };
 }
 
 function parse (line) {
